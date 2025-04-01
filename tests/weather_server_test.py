2cfffb8f885bfc4773b98ee9ae1e14887a20e4ae
Joey B <joeyb@Joeys-MacBook-Pro.local>
2025-03-31 21:33:53 -0600

    Adding tests for server endpoints

---
+++ tests/weather_server_test.py
@@ -0,0 +1,47 @@
+import pytest
+from ..weather_server import create_app
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    yield app
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+    
+    
+def test_validate_forecast_request():
+    """
+    Test the forecast request with valid latitude and longitude.
+    """
+    client = create_app().test_client()
+    lattitude = 39.7456
+    longitude = -97.0892
+    forecast_link = f"forecast/{lattitude},{longitude}"
+    forecast_request = client.get(forecast_link)
+    assert forecast_request.status_code == 200, f"Expected status code 200, but got {forecast_request.status_code}"
+    assert "Short Forecast:" in forecast_request.text
+    assert "Tempature Characterization:" in forecast_request.text
+    
+def test_invalid_forecast_request():
+    """
+    Test the forecast request with invalid latitude and longitude.
+    """
+    client = create_app().test_client()
+    lattitude = "123"
+    longitude = "456"
+    forecast_link = f"forecast/{lattitude},{longitude}"
+    forecast_request = client.get(forecast_link)
+    assert forecast_request.status_code == 500, f"Expected status code 500, but got {forecast_request.status_code}"
+    assert "Unable to fetch weather data" in forecast_request.text, f"Expected error message not found in response: {forecast_request.text}"    
+    
+def test_index_page():
+    """
+    Test the index page.
+    """
+    client = create_app().test_client()
+    index_request = client.get('/')
+    assert index_request.status_code == 200, f"Expected status code 200, but got {index_request.status_code}"
+    assert "Welcome to the Weather Server!" in index_request.text, f"Expected welcome message not found in response: {index_request.text}"
